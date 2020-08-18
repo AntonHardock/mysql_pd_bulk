9c5e383ebddc4ab1c462ec4bc2e7b0329e39f654
Anton Hardock <antonhardock@web.de>
2020-08-18 20:29:50 +0200

    specify multiple foreign keys
        Each foreign key is parsed from a nested dictionary
        in the table specification dictionary.

--- bulk_insert.py
+++ bulk_insert.py
@@ -9,38 +9,48 @@ def define_columns(k, v):
     '''Returns a string defining column names and their datatypes in SQL.
     Key:Value pairs are interpreted as column_name:datatype.
     If the key is a tuple listign multiple column names, all column names get the same datatype.
-        '''
+    '''
     if type(k) is tuple:
         return ", ".join("{} {}".format(col, v) for col in k)
     else: 
         return "{} {}".format(k, v)
 
-def define_table(table_specs):
-    """Returns a string defining a SQL table"""
-    col_statement = ", ".join(define_columns(k, v) for k, v in table_specs.items() if k is not "fk")
+def define_foreign_keys(fk_dict):
+    """Returns a string defining foreign keys for SQL table.
+    Name of the table constraint, as well as behaviour on update and delete
+    are optional, like in MySQL
+    """
+    fk_statement = fk_declaration.format(fk_dict)
+    if "name" in fk_dict:
+        fk_statement = fk_name.format(fk_dict) + fk_statement
+    if "update" in fk_dict:
+        fk_statement = fk_statement + fk_update.format(fk_dict)
+    if "delete" in fk_dict:
+        fk_statement = fk_statement + fk_delete.format(fk_dict)
+    return fk_statement
 
-    if "fk" in table_specs:
-        fk = table_specs.pop("fk")
-        fk_statement = fk_declaration.format(fk)
-        if "name" in fk:
-            fk_statement = fk_name.format(fk) + fk_statement
-        if "update" in fk:
-            fk_statement = fk_statement + fk_update.format(fk)
-        if "delete" in fk:
-            fk_statement = fk_statement + fk_delete.format(fk)
-        col_statement = col_statement + "," + fk_statement
-        
-    return col_statement
+def define_table(table_name, table_specs):
+    """Returns a string defining a SQL table"""
+
+    col_definitions = {k: v for k, v in table_specs.items() if not isinstance(v, dict)}
+    fk_definitions = {k :v for k,v in table_specs.items() if isinstance(v, dict)}
+
+    sql_code = ", ".join(define_columns(k, v) for k, v in col_definitions.items() if k is not "fk")
+    if fk_definitions:  #proceed if dict is not empty
+        fk_statements = [define_foreign_keys(fk) for fk in fk_definitions.values()]
+        sql_code = ",".join([sql_code] + fk_statements)
+    table_definition = "CREATE TABLE {} ({})".format(table_name, sql_code)
+
+    return table_definition
 
 def insert_table(cursor, reader, table_name, table_specs):
     """Bulk insert a reader object into a table."""
 
     ## generate sql create statement
-    table_string = generate_table_specs(table_specs)
-    table_string = "CREATE TABLE {} ({})".format(table_name, table_string)
+    table_definition = define_table(table_name, table_specs)
 
     ## create table 
-    cursor.execute(table_string)
+    cursor.execute(table_definition)
 
     ## generate sql insert statement
     col_names = ", ".join(table_specs.keys())
@@ -66,7 +76,6 @@ def insert_multiple_tables(cursor, reader_func, insert_instructions):
         
         path = instructions[0]
         column_dtype_map = instructions[1]
-
         reader = reader_func(path)
 
         insert_table(cursor, reader, table_name, column_dtype_map)
