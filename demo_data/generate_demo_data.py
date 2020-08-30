5e2299451383e99970ba6876803d0b6f017c012d
Anton Hardock <antonhardock@web.de>
2020-08-30 14:27:53 +0200

    finalize script generating demo data
    Adjusted the script so that main arguments can be passed
    from a command line.

--- demo_data/generate_demo_data.py
+++ demo_data/generate_demo_data.py
@@ -1,49 +1,55 @@
 import string
 import random as rd
 import pandas as pd
+import os
+import sys
+
+## change output path to desired location
+path = sys.argv[1]
+n = int(sys.argv[2])
 
 ## function to create dummy data
-def generate_dummy_data(n=1000):
+def generate_dummy_data(n=1000, path=None):
 
-    rd.seed(368201)
+        rd.seed(368201)
 
-    letters = string.ascii_letters
-    letters_up = string.ascii_uppercase
+        letters = string.ascii_letters
+        letters_up = string.ascii_uppercase
 
-    ## generic id variable
-    generic_id = range(1,n+1)
+        ## generic id variable
+        generic_id = range(1,n+1)
 
-    ## generate table: users
-    user_id = generic_id
-    username = ("".join(rd.choices(letters, k=rd.randint(4, 20))) for i in range(n))
+        ## generate table: users
+        user_id = generic_id
+        username = ("".join(rd.choices(letters, k=rd.randint(4, 20))) for i in range(n))
 
-    data = {"user_id": user_id, "username": username}
-    data = pd.DataFrame(data)
-    data.to_csv("users.csv", index=False)
+        data = {"user_id": user_id, "username": username}
+        data = pd.DataFrame(data)
+        data.to_csv(path + "users.csv", index=False)
 
-    ## generate table: products
-    product_id = generic_id
-    category = (rd.choice(letters_up) for i in range(n))
-    price = (rd.gauss(100, 30) + 50 for i in range(n))
-    weight = (rd.expovariate(0.3) for i in range(n))
+        ## generate table: products
+        product_id = generic_id
+        category = (rd.choice(letters_up) for i in range(n))
+        price = (rd.gauss(100, 30) + 50 for i in range(n))
+        weight = (rd.expovariate(0.3) for i in range(n))
 
-    data = {"product_id": product_id, "category": category,
-            "price": price, "weight": weight}
-    data = pd.DataFrame(data)
-    data.to_csv("products.csv", index=False)
+        data = {"product_id": product_id, "category": category,
+                "price": price, "weight": weight}
+        data = pd.DataFrame(data)
+        data.to_csv(path + "products.csv", index=False)
 
-    ## generate table: reviews
-    review_id = generic_id
-    fk_user_id = rd.choices(user_id, k=n)
-    fk_product_id = rd.choices(product_id, k=n)
-    review_text = (" ".join(["text"] * rd.randint(10, 50)) for i in range(n))
+        ## generate table: reviews
+        review_id = generic_id
+        fk_user_id = rd.choices(user_id, k=n)
+        fk_product_id = rd.choices(product_id, k=n)
+        review_text = (" ".join(["text"] * rd.randint(10, 50)) for i in range(n))
 
-    data = {"review_id": review_id, "user_id": fk_user_id,
-            "product_id": fk_product_id, "review_text": review_text}
-    data = pd.DataFrame(data)
-    data.to_json("reviews.json", orient="records", lines=True)
+        data = {"review_id": review_id, "user_id": fk_user_id,
+                "product_id": fk_product_id, "review_text": review_text}
+        data = pd.DataFrame(data)
+        data.to_json(path + "reviews.json", orient="records", lines=True)
 
-## export 
-generate_dummy_data(n=1000)
+## choose number of rows in each table (n) and export 
+generate_dummy_data(n=n, path=path)
 
 
