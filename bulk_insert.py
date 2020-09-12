a8c8e7be375b8e7e770d69d1ef77412561a8e885
Anton Hardock <antonhardock@web.de>
2020-09-12 17:23:05 +0200

    finalize foreign key checking
    Function "check_foreign_key" now asserts all
    required preconditions with proper error messages.
    Also, it is now properly integrated into
    "insert_table" and "insert_multiple_tables".

--- bulk_insert.py
+++ bulk_insert.py
@@ -104,60 +104,83 @@ def define_foreign_keys(table_name, table_specs):
 
 def check_foreign_key(cursor, table_name, table_specs):
     """Fast foreign key integrity check after batch upload
-       CAUTION: only works for INTEGER columns so far
 
     The referenced column from the parent table has to meet following conditions:
-    a) it is a Primary Key (ensuring that it's values are unique) 
-    b) all distinct values between MIN and MAX are present 
-       (MAX value of Primary Key = number of rows in parent table)
+    a) datatype INT
+    b) it is defined as Primary Key (ensuring that it's values are unique) 
+    c) all distinct values between MIN and MAX are present 
+       (such that MAX(Primary Key) = number of rows in table)
 
-   If conditions are met, integrity checking works as follows:
-    get MIN and MAX from referenced PK
-    ensure all distinct values between MIN and MAX are present in PK (COUNT(PK) == MAX(PK))
-    finally:
-        select unique values of FK column
-        check if ALL values lie between MIN and MAX of PK"""
+    Then integrity checking can be simplified to:
+    Check if ALL values lie between MIN and MAX of Primary Key"""
     
     fk_definitions = {k:v for k,v in table_specs.items() if isinstance(v, dict)}
     
     for fk in fk_definitions.values():
         
-        fk_column = fk["col"]
-        print("Fast integrity check of FK {} in table {}...".format(fk_column, table_name))
+        fk_col, ref = fk["col"], fk["ref"]
+        print("Fast integrity check of FK {} in table {}...".format(fk_col, table_name))
 
-        ## parent table and referenced column 
-        reference = fk["ref"]
-        start = reference.find("(")
-        end = reference.find(")")
-        parent_table = reference[:start]
-        parent_key = reference[start + 1:end]
+        ## parse referenced table and column to separate variables
+        start, end = ref.find("("), ref.find(")")
+        ref_tab = ref[:start] #name of referenced table 
+        ref_col = ref[start + 1:end] #name of referenced column
+        
+        ## retrieve Min and Max of referenced column
+        cursor.execute("SELECT MIN({1}), MAX({1}) FROM {0}".format(ref_tab, ref_col))
+        ref_min, ref_max = cursor.fetchall()[0]
 
-        ## retrieve Min and Max of referenced PK 
-        query_input = [parent_key, parent_table]
-        pk_boundaries = "SELECT MIN({0}), MAX({0}) FROM {1}".format(*query_input)
-        cursor.execute(pk_boundaries)
-        pk_min, pk_max = cursor.fetchall()[0]
+        ## query num of rows in referenced table + datatype and key status of referenced column
+        cursor.execute("DESCRIBE {0} {1}".format(ref_tab, ref_col))
+        result = cursor.fetchall()[0]
+        dtype, key_status = result[1], result[3]
 
-        ## assert that all values between Min and Max of referenced PK are present
-        pk_count = "SELECT COUNT({}) FROM {}".format(parent_key, parent_table)
-        cursor.execute(pk_count)
-        pk_count = cursor.fetchall()[0][0]
-        error_msg = "In {}, not all values are present between MIN and MAX of {}".format(parent_table, parent_key)
-        assert (pk_count == pk_max), error_msg
+        cursor.execute("SELECT COUNT(*) FROM {0}".format(ref_tab))
+        ref_tab_count = cursor.fetchall()[0][0]
+        
+        ## assert the conditions mentioned in the docstring are met
+        error_msg = "Table {0}: {1} is not integer".format(ref_tab, ref_col)
+        assert(dtype == "int"), error_msg
 
-        ## assert that all unique/distinct FK values are inside PK value range
-        query_input = [fk_column, table_name, pk_min, pk_max]
-        vals_offlimit = "SELECT COUNT(DISTINCT {0}) FROM {1} WHERE {0} NOT BETWEEN {2} AND {3}".format(*query_input)
-        cursor.execute(vals_offlimit)
-        vals_offlimit = cursor.fetchall()[0][0]
-        error_msg = "Foreign key contains values that do not occur in the referenced column "
-        assert (vals_offlimit == 0), error_msg
+        error_msg = "Table {0}: {1} is not a Primary Key".format(ref_tab, ref_col)
+        assert(key_status == "PRI"), error_msg
+
+        error_msg = "Table {0}: discrete values between MIN and MAX of {1} are missing".format(ref_tab, ref_col)
+        assert (ref_tab_count == ref_max), error_msg
+
+        ## Finally, check integrity of foreign key:
+        query = """SELECT COUNT(DISTINCT {0}) FROM {1} 
+                WHERE {0} NOT BETWEEN {2} AND {3}"""
+        cursor.execute(query.format(fk_col, table_name, ref_min, ref_max))
+        n_offlimit = cursor.fetchall()[0][0]
+        error_msg = "Table {0}: {1} contains values unmatched by '{2}'"
+        error_msg = error_msg.format(table_name, ref_col, ref)
+        assert (n_offlimit == 0), error_msg
+    
+    if fk_definitions.values():
+        print("FK integrity check completed")
+    else:
+        print("No foreign key definitions to check")
 
 ## _________________________________________________________________________________
 ## insert tables to MySQL Server through mysql.connector
 
-def insert_table(cursor, reader, table_name, table_specs, fast_fk_integrity_check = False):
-    """Bulk / chunkwise insert a pandas reader object as MySQL table."""
+def insert_table(cursor, reader, table_name, table_specs, fast_fk_check = False):
+    """Bulk / chunkwise insert a pandas reader object as MySQL table.
+    Foreign keys are added AFTER table creation and insertion.
+    Make sure the parent keys are indexed first (usually as Primary Keys)
+    
+    With very large tables, integrity checks of foreign keys may take hours!
+    Use fast_fk_check = True to use an abbreviated procedure, BUT ONLY IF 
+    all referenced columns meet the following conditions:
+        a) datatype INT
+        b) already defined as Primary Key (ensuring unique values) 
+        c) all distinct values between MIN and MAX are present 
+           (such that MAX(Primary Key) = number of rows in table)
+    Then, and only then, integrity checking can be simplified to:
+        Check if all UNIQUE values of the foreign key candidate 
+        fall between MIN and MAX of the Primary Key in the parent table.
+    """
 
     ## create required sql statements and initialize table
     table_definition = define_table(table_name, table_specs)
@@ -166,39 +189,47 @@ def insert_table(cursor, reader, table_name, table_specs, fast_fk_integrity_chec
 
     ## insert table chunkwise from pandas reader object
     for chunk in tqdm(reader):
-        chunk = chunk.loc[:, col_names] #reduce chunk to specified columns
+        try: 
+            chunk = chunk.loc[:, col_names] #reduce chunk to specified columns
+        except KeyError:
+            raise KeyError("Check if column names match between dataset and table_specs") 
         values = chunk.to_records(index=False).tolist()
         cursor.executemany(insert_definition, values)
 
-    ## OPTIONAL: fast FK integrity check, disables automatic checks
-    if fast_fk_integrity_check:
+    ## add foreign keys if foreign key definitions are provided
+    fk_definitions = define_foreign_keys(table_name, table_specs)
+
+    if fast_fk_check:
         cursor.execute("SET FOREIGN_KEY_CHECKS=0")
         check_foreign_key(cursor, table_name, table_specs)
 
-    ## add foreign keys if foreign key definitions are provided
-    fk_definitions = define_foreign_keys(table_name, table_specs)
     if fk_definitions:
         print("adding foreign keys...")
-        print(datetime.now())
-        for fk in fk_definitions: cursor.execute(fk)
-        print("foreign keys added")
-        print(datetime.now())
+        for fk in fk_definitions:
+            now = datetime.now().strftime("%H:%M:%S")
+            print("Add foreign key at: " + now)
+            cursor.execute(fk)
+            now = datetime.now().strftime("%H:%M:%S")
+            print("Foreign key added at: " + now)
+        print("Foreign key constraints are now in place")
 
-    ## After fast FK integrity check: enable automatic FK checks again
-    if fast_fk_integrity_check:
+    if fast_fk_check:
         cursor.execute("SET FOREIGN_KEY_CHECKS=1")
 
-def insert_multiple_tables(cursor, insert_instructions):
-    """Uses "insert_table" for multiple input tables, iterating
-    over "insert_instructions" that are structured like this:
-    
+def insert_multiple_tables(cursor, insert_instructions, fast_fk_check = False):
+    """Wrapper for multiple use of  "insert_table".
+    Itereates over "insert_instructions" that need to be structured like this:
+    Check docstring of "insert_table" for an explanation of
+    keyword argument "fast_fk_check".
+
     insert_instructions = {
-        "table_a": (path_to_a, reader_function_x, tablespecs_a),
-        "table_b": (path_to_b, reader_function_y, tablespecs_b)
+        "table_name_a": (path_to_a, reader_function_a, tablespecs_a),
+        "table_name_b": (path_to_b, reader_function_b, tablespecs_b)
     }
     """
     for table_name, instructions in insert_instructions.items():
         fpath, reader_function, table_specs = instructions
         reader = reader_function(fpath)
-        insert_table(cursor, reader, table_name, table_specs)
+        insert_table(cursor, reader, table_name, table_specs,
+                     fast_fk_check = fast_fk_check)
 
